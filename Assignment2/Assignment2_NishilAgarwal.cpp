44f3882b0f3558459b468c3d3e0950b4a41802dc
Nishil-Agarwal <132306904+Nishil-Agarwal@users.noreply.github.com>
2024-02-11 21:37:26 +0530

    Update Assignment2_NishilAgarwal.cpp

--- Assignment2/Assignment2_NishilAgarwal.cpp
+++ Assignment2/Assignment2_NishilAgarwal.cpp
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-void solve(ll n, vector<ll> booth) {
-    vector<vector<ll>> dp(n, vector<ll>(2, 0));
+void solve(int n, vector<int> booths){
+    vector<int> dp_end_at_ding(n,0);
+    vector<int> dp_end_at_ring(n,0);
 
-    dp[0][0] = booth[0];
-    dp[0][1] = -booth[0];
-    dp[1][0] = max(booth[0], -booth[0])+booth[1];
-    dp[1][1] = max(-booth[0], booth[0])-booth[1];
-    dp[2][0] = max(booth[0]+booth[1],max(dp[0][1]+booth[1],dp[1][1]))+booth[2];
-    dp[2][1] = max(-booth[0]-booth[1],max(dp[0][0]-booth[1],dp[1][0]))-booth[2];
+    dp_end_at_ring[0] = booths[0];
+    dp_end_at_ding[0] = -booths[0];
+    dp_end_at_ring[1] = max(dp_end_at_ring[0], dp_end_at_ding[0])+booths[1];
+    dp_end_at_ding[1] = max(dp_end_at_ring[0], dp_end_at_ding[0])-booths[1];
+    dp_end_at_ring[2] = max(dp_end_at_ring[1], dp_end_at_ding[1])+booths[2];
+    dp_end_at_ding[2] = max(dp_end_at_ring[1], dp_end_at_ding[1])-booths[2];
 
-    for (ll i = 3; i < n; i++) {
-        dp[i][0] = max(dp[i-1][1], max(dp[i-2][1] + booth[i-1], dp[i-3][1] + booth[i-2]+ booth[i-1])) +booth[i];
-        dp[i][1] = max(dp[i-1][0], max(dp[i-2][0]-booth[i-1], dp[i-3][0]-booth[i-2]-booth[i-1])) -booth[i];
+    for (int i=3; i<n; i++) {
+        dp_end_at_ring[i] = max(dp_end_at_ding[i-1], max(dp_end_at_ding[i-2]+booths[i-1], dp_end_at_ding[i-3]+booths[i-2]+booths[i-1]))+booths[i];
+        //If the dp is ending at 'Ring' at Ith element, it must follow that at least 1 out of i-1,i-2,i-3th element is a Ding. Similarly for ending at Ding case :
+        dp_end_at_ding[i] = max(dp_end_at_ring[i-1], max(dp_end_at_ring[i-2]-booths[i-1], dp_end_at_ring[i-3]-booths[i-2]-booths[i-1]))-booths[i];
     }
 
-    cout << max(dp[n-1][0], dp[n-1][1]);
+    cout << max(dp_end_at_ring[n-1], dp_end_at_ding[n-1])<<endl;
 }
 
-int main() {
-    vector<ll> booth = {1,9,9,-1,9,9,1,9,9};
-    solve(booth.size(), booth);
+int main(){
+    vector<int> booths = {1,9,9,-1,9,9,1,9,9};
+    solve(booths.size(), booths);
 }
