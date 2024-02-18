912765c1ed833233fca85e6396805c025e4cc845
Himang Garg <130616649+himangg@users.noreply.github.com>
2024-02-18 22:59:30 +0530

    Optimized the time complexity.
    (mn)^2 to (m+n)mn.

--- Assignment3/Assignment3_HimangGarg.cpp
+++ Assignment3/Assignment3_HimangGarg.cpp
@@ -14,21 +14,11 @@ void solve(ll m,ll n, vector<vector<ll>> v) {
     for(int h=0;h<m;h++) {
         for (int i = 0; i < n; i++) {
             vector<ll> temp = {v[h][i]};
-            for (int j = -1; j < i; j++) {
-                for( int k=-1 ; k<h ; k++ ) {
-                    if(j==-1 && k==-1){
-                        continue;
-                    }
-                    else if(k==-1) {
-                        temp.push_back(dp[h][j] + dp[h][i - j - 1]);
-                    }
-                    else if(j==-1){
-                        temp.push_back(dp[k][i] + dp[h-k-1][i]);
-                    }
-                    else{
-                        temp.push_back(dp[k][j]+dp[h-k-1][j]+dp[k][i-j-1]+dp[h-k-1][i-j-1]);
-                    }
-                }
+            for( int k=0 ; k<h ; k++ ) {
+                temp.push_back(dp[k][i]+dp[h-k-1][i]);
+            }
+            for (int j=0; j < i; j++) {
+                temp.push_back(dp[h][j]+dp[h][i-j-1]);
             }
             dp[h][i] = *max_element(temp.begin(), temp.end());
         }
@@ -39,14 +29,13 @@ void solve(ll m,ll n, vector<vector<ll>> v) {
             cout<<dp[h][i]<<' ';
         }
     }
-
     cout<<endl<<"ANSWER: "<<dp[m-1][n-1];
 }
 
 int main() {
     int m,n;
-    m=2;n=3;
-    vector<vector<ll>> v={{2,4,1},{4,1,3}};
-    solve(m,n, v);
+    m=3;n=3;
+    vector<vector<ll>> v={{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    solve(m,n,v);
     return 0;
 }
